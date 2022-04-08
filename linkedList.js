2830243088fb6f05b533ada4f60f642bad9b2d7e
LivCodes <oliviareeddev@gmail.com>
2022-04-07 21:15:21 -0400

    initial commit

---
+++ linkedList.js
@@ -0,0 +1,18 @@
+// Linked Lists
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+
